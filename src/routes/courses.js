e7e1fedb99d63c17d1e8dccbfc4947b33bd271e3
felipemir <robsonfelipeprog@gmail.com>
2025-07-10 11:23:36 -0400

    feat 3

--- src/routes/courses.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllCourses } = require('../controllers/courseController');
-
-router.get('/', getAllCourses);
-
-module.exports = router;
